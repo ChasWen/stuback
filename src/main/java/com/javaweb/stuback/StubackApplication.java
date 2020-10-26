8a5c61a771a9c4c0f39ec5b466bc5b9a8374bae3
ChasWen <chaswen@163.com>
2020-10-26 12:22:00 +0800

    第一次上传

---
+++ src/main/java/com/javaweb/stuback/StubackApplication.java
@@ -0,0 +1,13 @@
+package com.javaweb.stuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StubackApplication.class, args);
+    }
+
+}
